9751b8435c056c35e03fc302316e825b9afb734a
seanmlee <82421211+seanmlee@users.noreply.github.com>
2024-05-29 13:55:53 -0400

    `00_source.R`

---
+++ scripts/00_source.R
@@ -0,0 +1,12 @@
+source("scripts/01_load_puf.R")
+source("scripts/02_load_thy.R")
+source("scripts/03_load_mg.R")
+
+source("scripts/04a_prep_logit.R")
+source("scripts/04c_analysis_logit.R")
+
+source("scripts/05a_prep_rate.R")
+source("scripts/05b_analysis_rate.R")
+
+source("scripts/06a_prep_rate_minimal.R")
+source("scripts/06b_analysis_rate_minimal.R")
